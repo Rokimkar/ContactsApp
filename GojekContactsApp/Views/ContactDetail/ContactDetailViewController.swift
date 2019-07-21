8e5701ac916829dec7e8f9f4d3f8991b5c7c2eea
Sanchit Kumar Singh <sanchit.singh@timesinternet.in>
2019-07-21 16:47:20 +0530

    contact detail initial setup

---
+++ GojekContactsApp/Views/ContactDetail/ContactDetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ContactDetailViewController.swift
+//  GojekContactsApp
+//
+//  Created by S@nchit on 21/07/19.
+//  Copyright © 2019 self. All rights reserved.
+//
+
+import UIKit
+
+class ContactDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
